faef777da99c92354429953b5b647e2d6adc7eaf
Harshvardhan <shreeharshpb11@gmail.com>
2024-05-27 23:33:04 +0530

    complete review UI page

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,9 +1,56 @@
-
+import { review } from "../utils/review";
+import front from "../assets/asset 9.png";
+import reviewBanner from "../assets/review-banner-03.jpg";
+import { FcRating } from "react-icons/fc";
 
 const Review = () => {
   return (
-    <div>Review</div>
-  )
-}
+    <div>
+      <div className="absolute top-0 -z-10">
+        <img src={reviewBanner} alt=""  />
+      </div>
+      <div>
+        <img
+          src={front}
+          alt=""
+          className="w-full h-[550px] absolute opacity-55 top-0 -z-10"
+        />
+      </div>
+      <h1 className="mx-[160px] my-[130px] text-[100px] text-white">Reviews</h1>
+      <div className="mx-[171px] mt-[300px]">
+        <h1 className="text-[75px] text-[#414042] ">What Our Client Say</h1>
+        <div className="flex flex-wrap justify-between">
+          {review.map((rev) => (
+            <div key={rev.id} className="w-[50%] mt-[50px] mb-[50px]">
+              <p className="text-[18px] font-workSans text-[#414042] font-bold mt-[10px]">
+                {rev.review}
+              </p>
+              <div className="flex mt-[25px]">
+                <span>
+                  <FcRating />
+                </span>
+                <span>
+                  <FcRating />
+                </span>
+                <span>
+                  <FcRating />
+                </span>
+                <span>
+                  <FcRating />
+                </span>
+                <span>
+                  <FcRating />
+                </span>
+              </div>
+              <h1 className="text-[32px] font-handwriting mt-[35px]">
+                {rev.name}
+              </h1>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Review
+export default Review;
---
+++ src/utils/review.js
@@ -0,0 +1,40 @@
+import { FcRating } from "react-icons/fc";
+
+export const review = [
+    {
+        id: 1,
+        name: 'Willie Stone',
+        rating :FcRating,
+        review : " Aenean sollicitudin, quis auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio. "
+    },
+    {
+        id: 2,
+        name: 'Patrick Ray',
+        rating :FcRating,
+        review : "Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio amet nibh cursus a sit amet mauris."
+    },
+    {
+        id: 3,
+        name: 'Craig Hughes',
+        rating :FcRating,
+        review : "Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio amet nibh cursus a sit amet mauris."
+    },
+    {
+        id: 4,
+        name: 'Henry Nichols',
+        rating :FcRating,
+        review : " Aenean sollicitudin, quis auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio. "
+    },
+    {
+        id: 5,
+        name: 'Mary Bishop',
+        rating :FcRating,
+        review : " Aenean sollicitudin, quis auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio. "
+    },
+    {
+        id: 6,
+        name: 'Kevin George',
+        rating :FcRating,
+        review : "Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio amet nibh cursus a sit amet mauris."
+    },
+]
