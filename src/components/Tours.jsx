33a0ce5c5f012fa7cd843a8c3c29e86c303cf2df
Harshvardhan <shreeharshpb11@gmail.com>
2024-05-27 21:25:25 +0530

    complete tour UI page

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,9 +1,45 @@
-
+import tourBanner from "../assets/tour-banner-01.jpg";
+import front from "../assets/asset 9.png";
+import { tour } from "../utils/tours";
+import { FaArrowAltCircleRight } from "react-icons/fa";
 
 const Tours = () => {
   return (
-    <div>Tours</div>
-  )
-}
+    <div>
+      <div className="absolute top-0 -z-10">
+        <img src={tourBanner} alt="" />
+      </div>
+      <div>
+        <img
+          src={front}
+          alt=""
+          className="w-full h-[550px] absolute opacity-35 top-0 -z-10"
+        />
+      </div>
+      <h1 className="mx-[160px] my-[130px] text-[100px] text-white">Tours</h1>
+      <div className="mx-[171px] mt-[300px]">
+        <h1 className="text-[75px] text-gray-700 ">Top Destination</h1>
+        <div className="flex flex-wrap justify-between">
+          {tour.map((tour) => (
+            <div key={tour.id} className="w-[33.33%] mt-[50px] mb-[50px]">
+              <img src={tour.image} alt="" className="w-[400px] h-[300px]" />
+              <h1 className="text-[32px] font-handwriting mt-[35px]">
+                {tour.name}
+              </h1>
+              <h1 className="text-[18px] font-workSans">{tour.text}</h1>
+              <span className="text-[18px] font-workSans">{tour.price}</span>
+              <p className="text-[18px] font-workSans  mt-[10px]">
+                {tour.desc}
+              </p>
+              <button className="flex items-center text-white gap-4 mt-[40px] py-2 px-10 uppercase font-sans bg-[#4ca0af] rounded-md">
+                View Details <FaArrowAltCircleRight className="text-white" />
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Tours
+export default Tours;
---
+++ src/utils/tours.js
@@ -0,0 +1,85 @@
+import image1 from '../assets/tours-17-free-img.jpg'
+import image2 from '../assets/travel-img-02.jpg'
+import image3 from '../assets/travel-img-03.jpg'
+import image4 from '../assets/travel-img-04.jpg'
+import image5 from '../assets/travel-img-05.jpg'
+import image6 from '../assets/travel-img-06.jpg'
+import image7 from '../assets/travel-img-07.jpg'
+import image8 from '../assets/travel-img-08.jpg'
+import image9 from '../assets/tours-13-free-img.jpg'
+
+export const tour = [
+    {
+        id: 1,
+        name: 'Maldives',
+        text: 'Sunny Ari Atoll',
+        price: '$2350.00',
+        desc: 'Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.',
+        image: image1
+    },
+    {
+        id: 2,
+        name: 'England',
+        text: 'Majestic Big Ben',
+        price: '$1635.00',
+        desc: 'Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.',
+        image: image2
+    },
+    {
+        id: 3,
+        name: 'Italy',
+        text: 'Discover vince',
+        price: '$1550.00',
+        desc: 'Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.',
+        image: image3
+    },
+    {
+        id: 4,
+        name: 'Turky',
+        text: 'Historical Istanbul',
+        price: '$1250.00',
+        desc: 'Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.',
+        image: image4
+    },
+    {
+        id: 5,
+        name: 'France',
+        text: 'Amazing Eiffel Tower',
+        price: '$2350.00',
+        desc: 'Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.',
+        image: image5
+    },
+    {
+        id: 6,
+        name: 'Switzerland',
+        text: 'Faithful Swiss Alps',
+        price: '$1450.00',
+        desc: 'Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.',
+        image: image6
+    },
+    {
+        id: 7,
+        name: 'Spain',
+        text: 'Elegant Madrid',
+        price: '$1450.00',
+        desc: 'Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.',
+        image: image7
+    },
+    {
+        id: 8,
+        name: 'Spain',
+        text: 'Brave Barcelona',
+        price: '$1350.00',
+        desc: 'Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.',
+        image: image8
+    },
+    {
+        id: 9,
+        name: 'Brazil',
+        text: 'Tall Rio de Janeriro',
+        price: '$1350.00',
+        desc: 'Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.',
+        image: image9
+    },
+
+]
