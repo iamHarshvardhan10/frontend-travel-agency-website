7e1289c5b0b80548c6497633d620780ff6f135ba
Harshvardhan <shreeharshpb11@gmail.com>
2024-05-29 19:09:31 +0530

    complete responsiveness for aboutusComponent page ui for all device

--- src/components/AboutUsComponent.jsx
+++ src/components/AboutUsComponent.jsx
@@ -3,11 +3,15 @@ import aboutComoment from "../assets/asset 14.jpeg";
 
 const AboutUsComponent = () => {
   return (
-    <div className="flex items-center justify-between">
-      <img src={aboutComoment} alt="" />
-      <div className="p-20">
-        <span className="text-[32px] font-handwriting">a few words</span>
-        <h1 className="text-[75px] ">About Us</h1>
+    <div className="lg:flex lg:flex-row md:flex md:flex-col-reverse sm:flex sm:flex-col-reverse max-sm:flex max-sm:flex-col-reverse items-center justify-between">
+      <img src={aboutComoment} alt="" className="max-sm:w-full max-sm:p-2"/>
+      <div className="lg:p-20">
+        <span className="lg:text-[32px] md:text-[42px] sm:text-[42px] max-sm:text-[42px] font-handwriting">
+          a few words
+        </span>
+        <h1 className="lg:text-[75px] md:text-[100px] sm:text-[100px] max-sm:text-[75px]">
+          About Us
+        </h1>
         <p className="text-[18px] font-workSans">
           Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin,
           lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis
@@ -15,7 +19,7 @@ const AboutUsComponent = () => {
           amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio
           tincidunt auctor a ornare odio.
         </p>
-        <button className="flex items-center text-white gap-4 mt-[50px] py-2 px-10 uppercase font-sans bg-[#4ca0af] rounded-sm">
+        <button className="flex items-center text-white gap-4 lg:mt-[50px] md:mt-[30px] sm:mt-[30px] max-sm:mt-[20px] md:mb-[20px] sm:mb-[20px] max-sm:mb-[20px] py-2 px-10 uppercase font-sans bg-[#4ca0af] rounded-sm">
           View All Tours <FaArrowAltCircleRight className="text-white" />
         </button>
       </div>
